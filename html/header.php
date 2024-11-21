d087281d6a30d95f4b9a0f3f6711170a1a7cfa0d
SalazarSolanoEdy <edysalazar685@aragon.unam.mx>
2024-11-21 01:05:44 -0600

    Agregando icono a la pagina

--- html/header.php
+++ html/header.php
@@ -32,3 +32,38 @@
     </div>
   </nav>
 </header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="icon" href="./img/Logo_ico.png" type="image/png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <title>Titulación ICO</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="./css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="./css/styleMaterialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<header>
+  <nav>
+    <div class="nav-wrapper blue darken-4">
+      <img src="./img/logo_unam_oro.png" class="logo">
+      <img src="./img/Logo_fes_oro.png" class="logo">
+      <span class="brand-logo white-text">Titulación</span>
+      <ul id="nav-desktop" class="right hide-on-med-and-down">
+        <li><a href="./index.php" class="white-text"><i class="material-icons left">home</i>Inicio</a></li>
+        <li><a href="./formulario.php" class="white-text"><i class="material-icons left">assignment</i>Registro modalidad</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav grey lighten-1">
+        <li><a href="./index.php"><i class="material-icons left">home</i>Inicio</a></li>
+        <li><a href="./formulario.php"><i class="material-icons left">assignment</i>Registro</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+</header>
